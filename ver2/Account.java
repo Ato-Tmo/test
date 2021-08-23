5300618e332c5d8310bb60eb7d2e4e89210be589
Ato-Tmo <89419833+Ato-Tmo@users.noreply.github.com>
2021-08-23 16:40:59 -0400

    Added a new comment for balance

--- ver2/Account.java
+++ ver2/Account.java
@@ -2,6 +2,7 @@ package ver2;
 
 public class Account {
 	// Instance variable, only available inside this class.
+   // The balance should be over 1 dollar :)
 	private double balance;
 	private String name;
 	
